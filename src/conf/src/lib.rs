567211cf2c6067db5e08b71b2530d11d0aa9025b
Eric Driussi <ericdriussi@gmail.com>
2023-08-02 19:18:17 +0100

    update config file handling

--- src/conf/src/lib.rs
+++ src/conf/src/lib.rs
@@ -2,19 +2,29 @@ use std::env;
 
 use config::Config;
 
+const DEFAULT_CONFIG: &'static str = include_str!("configs/default.yaml");
+const TEST_CONFIG: &'static str = include_str!("configs/test.yaml");
+
 pub fn authors_file() -> String {
     return get_config().get::<String>("authors_file").unwrap();
 }
 
 fn get_config() -> Config {
-    let mut config_file = "src/conf/src/configs/default";
-    if let Ok(test_env) = env::var("COA_ENV") {
+    let config_file = if let Ok(test_env) = env::var("COA_ENV") {
         if test_env == "test" {
-            config_file = "../conf/src/configs/test";
+            TEST_CONFIG
+        } else {
+            DEFAULT_CONFIG
         }
-    }
+    } else {
+        DEFAULT_CONFIG
+    };
+
     return Config::builder()
-        .add_source(config::File::with_name(config_file))
+        .add_source(config::File::from_str(
+            config_file,
+            config::FileFormat::Yaml,
+        ))
         .build()
         .unwrap();
 }
