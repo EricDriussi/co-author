29262c34f7fb80a37bffaa3ed8583a0f6ef77dd9
Eric Driussi <ericdriussi@gmail.com>
2023-08-27 18:42:04 +0100

    rm editmsg conf

--- src/conf/tests/conf.rs
+++ src/conf/tests/conf.rs
@@ -5,9 +5,3 @@ fn should_return_expected_test_authors_file_path() {
 	let authors_file_path = PathBuf::from(conf::authors_file());
 	assert_eq!(authors_file_path.to_str().unwrap(), "src/authors/tests/data/authors");
 }
-
-#[test]
-fn should_return_expected_test_editmsg_file_path() {
-	let editmsg_file = conf::editmsg();
-	assert_eq!(editmsg_file, ".git/COMMIT_EDITMSG_TEST");
-}
