0d01cba95bdb64d9d473de7cd801699a340b7f7b
Eric Driussi <ericdriussi@gmail.com>
2023-10-22 11:25:36 +0100

    test for author equality

--- src/authors/test/author.rs
+++ src/authors/test/author.rs
@@ -1,4 +1,5 @@
 use crate::authors::author::Author;
+use parameterized::parameterized;
 
 fn setup_author() -> Author {
 	let alias = "a";
@@ -23,4 +24,21 @@ fn should_provide_an_alias_getter() {
 fn should_provide_a_name_getter() {
 	let author = setup_author();
 	assert_eq!(author.name(), "alice");
+}
+
+#[test]
+fn should_be_equal_to_another_author_with_equal_data() {
+	let author = Author::new("a", "alice", "alice@wonderland.not");
+	let same_author = Author::new("a", "alice", "alice@wonderland.not");
+	assert_eq!(author, same_author)
+}
+
+#[parameterized(different_author = {
+	Author::new("b", "alice", "alice@wonderland.not"),
+	Author::new("a", "not_alice", "alice@wonderland.not"),
+	Author::new("a", "alice", "someone@wonderland.not")
+})]
+fn should_not_be_equal_to_another_author_with_different_data(different_author: Author) {
+	let author = Author::new("a", "alice", "alice@wonderland.not");
+	assert_ne!(author, different_author)
 }
