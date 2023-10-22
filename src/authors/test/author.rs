a48f429f569c89a5d980aaa032c2f1749cad6a09
Eric Driussi <ericdriussi@gmail.com>
2023-10-22 11:11:50 +0100

    refactor author tests

--- src/authors/test/author.rs
+++ src/authors/test/author.rs
@@ -1,31 +1,26 @@
 use crate::authors::author::Author;
 
-#[test]
-fn should_present_the_correct_signature() {
+fn setup_author() -> Author {
 	let alias = "a";
 	let name = "alice";
 	let email = "alice@wonderland.not";
-	let author = Author::new(alias, name, email);
+	Author::new(alias, name, email)
+}
 
+#[test]
+fn should_present_the_correct_signature() {
+	let author = setup_author();
 	assert_eq!(author.signature(), "Co-authored-by: alice <alice@wonderland.not>");
 }
 
 #[test]
 fn should_provide_an_alias_getter() {
-	let alias = "a";
-	let name = "alice";
-	let email = "alice@wonderland.not";
-	let author = Author::new(alias, name, email);
-
+	let author = setup_author();
 	assert_eq!(author.alias(), "a");
 }
 
 #[test]
 fn should_provide_a_name_getter() {
-	let alias = "a";
-	let name = "alice";
-	let email = "alice@wonderland.not";
-	let author = Author::new(alias, name, email);
-
+	let author = setup_author();
 	assert_eq!(author.name(), "alice");
 }
