75cf5f6de0c92f249e932869c1cc324bcaedce05
Eric Driussi <ericdriussi@gmail.com>
2024-01-29 15:42:15 +0000

    minor refactor

--- src/cli/input_reader.rs
+++ src/cli/input_reader.rs
@@ -1,11 +1,9 @@
+use crate::Result;
 #[cfg(test)]
 use mockall::{automock, predicate::*};
 use rustyline::{error::ReadlineError, DefaultEditor};
-use std::result;
 
 use super::cli_err::CliError;
-
-type Result<T> = result::Result<T, CliError>;
 
 #[cfg_attr(test, automock)]
 pub trait Reader {
@@ -15,19 +13,20 @@ pub trait Reader {
 
 impl Reader for DefaultEditor {
 	fn readline(&mut self, prompt_msg: &str) -> Result<String> {
-		self.readline(prompt_msg).map_err(|e| match e {
+		Ok(self.readline(prompt_msg).map_err(|e| match e {
 			ReadlineError::Interrupted => CliError::Interrupted,
 			ReadlineError::Io(e) => CliError::Io(e),
 			_ => CliError::Unknown,
-		})
+		})?)
 	}
 
 	fn readline_with_initial(&mut self, prompt_msg: &str, pre_populate: (&str, &str)) -> Result<String> {
-		self.readline_with_initial(prompt_msg, pre_populate)
+		Ok(self
+			.readline_with_initial(prompt_msg, pre_populate)
 			.map_err(|e| match e {
 				ReadlineError::Interrupted => CliError::Interrupted,
 				ReadlineError::Io(e) => CliError::Io(e),
 				_ => CliError::Unknown,
-			})
+			})?)
 	}
 }
