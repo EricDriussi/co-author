aaa9013fe503dd85a0f1c2bb264fee102b5d3134
Eric Driussi <ericdriussi@gmail.com>
2024-03-08 08:10:19 +0000

    fix cli test for headfull terminals

--- src/cli/prompt_test.rs
+++ src/cli/prompt_test.rs
@@ -54,7 +54,7 @@ fn should_pretty_print_authors_when_prompting_for_aliases() {
 	let mut reader = MockReader::new();
 	reader
 		.expect_readline()
-		.withf(move |prompt_msg| prompt_msg.contains(format!("⦔ {alias} -> {name}").as_str()))
+		.withf(move |prompt_msg| contains_in_order(prompt_msg, &["⦔", alias, "->", name]))
 		.times(1)
 		.returning(move |_| Ok("whatever".to_string()));
 	let mut cli = Prompt::new(reader);
@@ -76,4 +76,13 @@ fn should_space_split_aliases() {
 	let result = cli.prompt_aliases(&[]);
 
 	assert!(matches!(result, Ok(aliases) if aliases == ["a", "b", "cd"]));
+}
+
+fn contains_in_order(prompt_msg: &str, components: &[&str]) -> bool {
+	// This is functionally equal to doing prompt_msg.contains("⦔ alias -> name")
+	// but only considers presence and relative order, discarding "colored" formatting (bold, colors, etc.)
+	let found: Vec<_> = components.iter().map(|comp| prompt_msg.find(comp)).collect();
+	let are_contained = found.iter().all(|&comp| comp.is_some());
+	let are_in_order = found.windows(2).all(|comp| comp[0] < comp[1]);
+	are_contained && are_in_order
 }
