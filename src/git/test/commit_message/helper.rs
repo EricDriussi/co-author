5f3b70f7f7f21a011ff395277002d28be7f9120a
Eric Driussi <ericdriussi@gmail.com>
2024-03-17 17:04:40 +0000

    better tests for commit message

--- src/git/test/commit_message/helper.rs
+++ src/git/test/commit_message/helper.rs
@@ -1,15 +1,67 @@
-pub const EMPTY: &str = "";
 pub const A_SUBJECT: &str = "subject line";
 pub const A_LINE: &str = "line";
 pub const AN_AUTHOR: &str = "Co-authored-by: author";
 pub const WHITESPACE: &str = " ";
 pub const COMMENT: &str = "#comment!";
-pub const NO_AUTHORS: Vec<String> = vec![];
 
-pub struct TestCase {
-	pub name: &'static str,
+pub struct TestMessage {
 	pub subject: String,
 	pub body: String,
 	pub authors: Vec<String>,
+}
+
+pub struct TestCase {
+	pub name: &'static str,
+	pub message: TestMessage,
 	pub expected: String,
+}
+
+impl TestCase {
+	pub fn build_for(name: &'static str) -> TestCaseBuilder {
+		TestCaseBuilder::new(name)
+	}
+}
+
+pub struct TestCaseBuilder {
+	test_case: TestCase,
+}
+
+impl TestCaseBuilder {
+	pub fn new(name: &'static str) -> Self {
+		TestCaseBuilder {
+			test_case: TestCase {
+				name,
+				message: TestMessage {
+					subject: "UNDEFINED".to_string(),
+					body: "UNDEFINED".to_string(),
+					authors: vec!["UNDEFINED".to_string()],
+				},
+				expected: "UNDEFINED".to_string(),
+			},
+		}
+	}
+
+	pub fn subject(mut self, subject: &str) -> Self {
+		self.test_case.message.subject = subject.to_string();
+		self
+	}
+
+	pub fn body(mut self, body: &str) -> Self {
+		self.test_case.message.body = body.to_string();
+		self
+	}
+
+	pub fn authors(mut self, authors: &[&str]) -> Self {
+		self.test_case.message.authors = authors.iter().map(|author| author.to_string()).collect();
+		self
+	}
+
+	pub fn expected(mut self, expected: &str) -> Self {
+		self.test_case.expected = expected.to_string();
+		self
+	}
+
+	pub fn create(self) -> TestCase {
+		self.test_case
+	}
 }
--- src/git/test/commit_message/should_create.rs
+++ src/git/test/commit_message/should_create.rs
@@ -3,85 +3,80 @@ use crate::git::commit_message::CommitMessage;
 
 fn test_cases() -> Vec<TestCase> {
 	vec![
-		TestCase {
-			name: "happy path",
-			subject: A_SUBJECT.to_string(),
-			body: format!("{A_LINE}\n{A_LINE}"),
-			authors: vec![A_LINE.to_string(), A_LINE.to_string()],
-			expected: format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}\n\n\n{A_LINE}\n{A_LINE}"),
-		},
-		TestCase {
-			name: "too many newlines",
-			subject: A_SUBJECT.to_string(),
-			body: format!("\n{A_LINE}\n\n\n{A_LINE}\n"),
-			authors: vec![A_LINE.to_string(), A_LINE.to_string()],
-			expected: format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}\n\n\n{A_LINE}\n{A_LINE}"),
-		},
-		TestCase {
-			name: "no body",
-			subject: A_SUBJECT.to_string(),
-			body: EMPTY.to_string(),
-			authors: vec![A_LINE.to_string(), A_LINE.to_string()],
-			expected: format!("{A_SUBJECT}\n\n\n{A_LINE}\n{A_LINE}"),
-		},
-		TestCase {
-			name: "no authors",
-			subject: A_SUBJECT.to_string(),
-			body: format!("{A_LINE}\n{A_LINE}"),
-			authors: NO_AUTHORS.clone(),
-			expected: format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}"),
-		},
-		TestCase {
-			name: "no body or authors",
-			subject: A_SUBJECT.to_string(),
-			body: EMPTY.to_string(),
-			authors: NO_AUTHORS.clone(),
-			expected: A_SUBJECT.to_string(),
-		},
-		TestCase {
-			name: "only authors",
-			subject: EMPTY.to_string(),
-			body: EMPTY.to_string(),
-			authors: vec![AN_AUTHOR.to_string(), AN_AUTHOR.to_string()],
-			expected: format!("\n\n\n{AN_AUTHOR}\n{AN_AUTHOR}"),
-		},
-		TestCase {
-			name: "empty",
-			subject: EMPTY.to_string(),
-			body: EMPTY.to_string(),
-			authors: NO_AUTHORS.clone(),
-			expected: EMPTY.to_string(),
-		},
-		TestCase {
-			name: "comment",
-			subject: A_SUBJECT.to_string(),
-			body: format!("{A_LINE}\n{COMMENT}\n{A_LINE}"),
-			authors: NO_AUTHORS.clone(),
-			expected: format!("{A_SUBJECT}\n\n{A_LINE}\n{COMMENT}\n{A_LINE}"),
-		},
-		TestCase {
-			name: "whitespaces",
-			subject: format!("{A_SUBJECT}{WHITESPACE}"),
-			body: format!("{A_LINE}{WHITESPACE}\n{A_LINE}"),
-			authors: NO_AUTHORS.clone(),
-			expected: format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}"),
-		},
+		TestCase::build_for("happy path")
+			.subject(A_SUBJECT)
+			.body(A_LINE)
+			.authors(&[AN_AUTHOR])
+			.expected(&format!("{A_SUBJECT}\n\n{A_LINE}\n\n\n{AN_AUTHOR}"))
+			.create(),
+		TestCase::build_for("too many newlines")
+			.subject(A_SUBJECT)
+			.body(&format!("\n{A_LINE}\n\n\n{A_LINE}\n"))
+			.authors(&[A_LINE, A_LINE])
+			.expected(&format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}\n\n\n{A_LINE}\n{A_LINE}"))
+			.create(),
+		TestCase::build_for("no body")
+			.subject(A_SUBJECT)
+			.body("")
+			.authors(&[A_LINE, A_LINE])
+			.expected(&format!("{A_SUBJECT}\n\n\n{A_LINE}\n{A_LINE}"))
+			.create(),
+		TestCase::build_for("no authors")
+			.subject(A_SUBJECT)
+			.body(&format!("{A_LINE}\n{A_LINE}"))
+			.authors(&[])
+			.expected(&format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}"))
+			.create(),
+		TestCase::build_for("no body or authors")
+			.subject(A_SUBJECT)
+			.body("")
+			.authors(&[])
+			.expected(&A_SUBJECT)
+			.create(),
+		TestCase::build_for("only authors")
+			.subject("")
+			.body("")
+			.authors(&[AN_AUTHOR, AN_AUTHOR])
+			.expected(&format!("\n\n\n{AN_AUTHOR}\n{AN_AUTHOR}"))
+			.create(),
+		TestCase::build_for("empty")
+			.subject("")
+			.body("")
+			.authors(&[])
+			.expected("")
+			.create(),
+		TestCase::build_for("comment")
+			.subject(A_SUBJECT)
+			.body(&format!("{A_LINE}\n{COMMENT}\n{A_LINE}"))
+			.authors(&[])
+			.expected(&format!("{A_SUBJECT}\n\n{A_LINE}\n{COMMENT}\n{A_LINE}"))
+			.create(),
+		TestCase::build_for("whitespaces")
+			.subject(&format!("{A_SUBJECT}{WHITESPACE}"))
+			.body(&format!("{A_LINE}{WHITESPACE}\n{A_LINE}"))
+			.authors(&[])
+			.expected(&format!("{A_SUBJECT}\n\n{A_LINE}\n{A_LINE}"))
+			.create(),
 	]
 }
 
 #[test]
 fn given_a_correctly_formatted_commit_message() {
-	for case in test_cases() {
-		let actual =
-			CommitMessage::new(&format!("{}\n\n{}", case.subject, case.body), case.authors.clone()).to_string();
-		assert_eq!(case.expected, actual, "{}", case.name);
+	for t in test_cases() {
+		let actual = CommitMessage::new(
+			&format!("{}\n\n{}", t.message.subject, t.message.body),
+			t.message.authors.clone(),
+		)
+		.to_string();
+		assert_eq!(t.expected, actual, "{}", t.name);
 	}
 }
 
 #[test]
 fn given_a_poorly_formatted_commit_message() {
-	for case in test_cases() {
-		let actual = CommitMessage::new(&format!("{}\n{}", case.subject, case.body), case.authors).to_string();
-		assert_eq!(case.expected, actual, "{}", case.name);
+	for t in test_cases() {
+		let actual =
+			CommitMessage::new(&format!("{}\n{}", t.message.subject, t.message.body), t.message.authors).to_string();
+		assert_eq!(t.expected, actual, "{}", t.name);
 	}
 }
