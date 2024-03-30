bfe27d19aa2e4ace05be491d46399e387c805074
Eric Driussi <ericdriussi@gmail.com>
2024-03-30 13:15:47 +0000

    rm file

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -2,14 +2,12 @@ pub mod conf;
 pub mod runner;
 
 pub mod fs {
-	pub mod file;
 	pub mod file_reader;
 	pub mod file_writer;
 
 	#[cfg(test)]
 	pub mod test {
 		mod file_reader_should;
-		mod file_should;
 		mod file_writer_should;
 
 		pub mod util {
